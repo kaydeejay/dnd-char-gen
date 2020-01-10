f81aa1d1668508bd48569190e57ff470fdddc52e
Kevin Devine-Jones <kevin.jones215@gmail.com>
2020-01-10 09:04:25 -0500

    displays rolled stats in appropriate divs

--- script.js
+++ script.js
@@ -1,18 +1,10 @@
 $(document).ready(function(){
     $(document).foundation();
 
-    // $("#randomStats").on("click", function(){
-    //     var rolledStatDivs = $("#rolledStats").children();
-    //     for (var i=0; i<rolledStatDivs.length; i++){
-    //         console.log($(rolledStatDivs[i]).children()[0]);
-    //         var currentStatEl = $(rolledStatDivs[i]).children()[0];
-    //         var currentStatVal = 
-    //     }
-    // });
-
     $("#randomStats").on("click", function(){
         var d6Vals = [];
         var statRolls = [];
+        var statRollTotals = [];
         $.ajax({
             // roll 24d6
             url: "http://roll.diceapi.com/json/24d6",
@@ -24,17 +16,39 @@ $(document).ready(function(){
             }
             // split them up into 6 rolls of 4, drop the lowest.
             while (d6Vals.length){
-                statRolls.push(removeSmallest(d6Vals.splice(0,4)));
+                statRolls.push(dropLowest(d6Vals.splice(0,4)));
             }
-            console.log(statRolls);
+            // reduce each sub-array to the total, and store them in statRollTotals.
+            for (var i=0; i< statRolls.length; i++){
+                statRollTotals.push(statRolls[i].reduce(getSum, 0));
+            }
+            console.log(statRollTotals);
+            populateRandStats(statRollTotals);
         });
     });
 
-    function removeSmallest(arr) {
+    function populateRandStats(arr){
+        var rolledStatDivs = $("#rolledStats").children();
+        for (var i=0; i<rolledStatDivs.length; i++){
+            // console.log($(rolledStatDivs[i]).children()[0]);
+            var currentStatEl = $(rolledStatDivs[i]).children()[0];
+            var currentStatVal = arr[i];
+            var dropDownText = $("<p>");
+            $(currentStatEl).text(currentStatVal);
+            $(dropDownText).text("Click to Assign a Stat");
+            $(currentStatEl).append(dropDownText);
+        }
+    }
+
+    function dropLowest(arr) {
         var min = Math.min.apply(null, arr);
         var minIndex = arr.indexOf(min);
         arr.splice(minIndex, 1);
         return arr;
+    }
+
+    function getSum(total, num) {
+      return total + num;
     }
 
 });
