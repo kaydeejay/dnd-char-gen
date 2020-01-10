a9654a204ab1d6d53f6ad0ffe489c0eb7713224c
Kevin Devine-Jones <kevin.jones215@gmail.com>
2020-01-10 01:07:25 -0500

    starts random rolls function

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 
     // $("#randomStats").on("click", function(){
     //     var rolledStatDivs = $("#rolledStats").children();
-    //     for (var i=0; i<rolledStatDivs.length-1; i++){
+    //     for (var i=0; i<rolledStatDivs.length; i++){
     //         console.log($(rolledStatDivs[i]).children()[0]);
     //         var currentStatEl = $(rolledStatDivs[i]).children()[0];
     //         var currentStatVal = 
@@ -11,16 +11,30 @@ $(document).ready(function(){
     // });
 
     $("#randomStats").on("click", function(){
-        var statRollVals = [];
+        var d6Vals = [];
+        var statRolls = [];
         $.ajax({
+            // roll 24d6
             url: "http://roll.diceapi.com/json/24d6",
             method: "GET"
         }).then(function(response){
-            var thisStatVal = [];
-            for (var i=0;i<6;i++){
-                thisStatVal.push(response.dice[])
+            // store all 24 rolls in an array
+            for (var i = 0; i < response.dice.length; i++){
+                d6Vals.push(response.dice[i].value);
             }
+            // split them up into 6 rolls of 4, drop the lowest.
+            while (d6Vals.length){
+                statRolls.push(removeSmallest(d6Vals.splice(0,4)));
+            }
+            console.log(statRolls);
         });
-    })
+    });
+
+    function removeSmallest(arr) {
+        var min = Math.min.apply(null, arr);
+        var minIndex = arr.indexOf(min);
+        arr.splice(minIndex, 1);
+        return arr;
+    }
 
 });
