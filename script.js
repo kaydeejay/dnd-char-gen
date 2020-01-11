bd4760eb9490b8033d27755271a0d569e7e81155
Kevin Devine-Jones <kevin.jones215@gmail.com>
2020-01-11 13:47:36 -0500

    bug fixes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+/* <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
 $.ajax({
     url: "https://api.open5e.com/classes/",
@@ -14,4 +14,4 @@ $.ajax({
     console.log(response);
   });
 
-
+ */
