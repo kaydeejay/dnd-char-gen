4f5481f429d4f56d8e0eb445fde8c80bff73ba40
Kevin Devine-Jones <kevin.jones215@gmail.com>
2020-01-11 00:31:12 -0500

    simplify/clarify button populate functions

--- script.js
+++ script.js
@@ -2,27 +2,11 @@ $(document).ready(function(){
     $(document).foundation();
 
     var randDropDownPanes = $(".rand-dd");
-    // var randStats = ["STR", "DEX", "CON", "INT", "WIS", "CHA"];
+    var randStatCalloutEls = $(".rand-callout")
+    var statTypes = ["STR", "DEX", "CON", "INT", "WIS", "CHA"];
 
-    addDropdownButtons();
-    function addDropdownButtons(){
-        var randStats = ["STR", "DEX", "CON", "INT", "WIS", "CHA"];
-        for (var i=0; i<randStats.length; i++){
-            var newStatButton = $("<button>");
-            $(newStatButton).addClass("button");
-            $(newStatButton).html(randStats[i]);
-            $(newStatButton).on("click", btnToStat);
-            $(randDropDownPanes).append(newStatButton);
-        }
-    }
-
-    function btnToStat(){
-        var chosenStat = $(this).html();
-        var ancestCallout = $(this).parent().parent().children()[0];
-        var uncleSpan = $(ancestCallout).children()[0];
-        $(uncleSpan).text(": " + chosenStat);
-    }
-
+    // add event listener to all callouts
+    $(randStatCalloutEls).on("click", populateButtons);
     // generate random stat values when the "roll" button is clicked
     $("#randomStats").on("click", function(){
         var d6Vals = [];
@@ -47,9 +31,10 @@ $(document).ready(function(){
             }
             // display the stat roll results in the "roll for stats" divs
             populateRandStats(statRollTotals);
+            // populateButtons(randDropDownPanes);
         });
     });
-
+    
     function populateRandStats(arr){
         var rolledStatDivs = $("#rolledStats").children();
         for (var i=0; i<rolledStatDivs.length; i++){
@@ -62,16 +47,49 @@ $(document).ready(function(){
             $(currentStatEl).append(dropDownText);
         }
     }
-
+    
     function dropLowest(arr) {
         var min = Math.min.apply(null, arr);
         var minIndex = arr.indexOf(min);
         arr.splice(minIndex, 1);
         return arr;
     }
-
+    
     function getSum(total, num) {
-      return total + num;
+        return total + num;
     }
- 
+
+    function populateButtons(){
+        // for (var i=0; i<obj.length; i++){}
+        console.log(this);
+        var btnVals = statTypes;
+        var siblingCallout = $(this).parent().children()[1];
+        console.log(btnVals);
+        for (var i=0; i<btnVals.length; i++){
+            var newStatButton = $("<button>");
+            $(newStatButton).addClass("button");
+            $(newStatButton).html(btnVals[i]);
+            $(siblingCallout).append(newStatButton);
+        }
+    }
+    
 });
+
+// addDropdownButtons();
+// function addDropdownButtons(){
+//     var randStats = ["STR", "DEX", "CON", "INT", "WIS", "CHA"];
+//     for (var i=0; i<randStats.length; i++){
+//         var newStatButton = $("<button>");
+//         $(newStatButton).addClass("button");
+//         $(newStatButton).html(randStats[i]);
+//         $(newStatButton).on("click", btnToStat);
+//         $(randDropDownPanes).append(newStatButton);
+//     }
+// }
+
+// function btnToStat(){
+//     var chosenStat = $(this).html();
+//     var ancestCallout = $(this).parent().parent().children()[0];
+//     var uncleSpan = $(ancestCallout).children()[0];
+//     $(uncleSpan).text(": " + chosenStat);
+// }
