8fe805c76a3e7077f52812b2fb54b17ca040b90d
Kevin Devine-Jones <kevin.jones215@gmail.com>
2020-01-13 09:55:38 -0500

    add new js file for save/continue functions and character sheet populating

---
+++ user-character.js
@@ -0,0 +1,25 @@
+// this js file will be linked to all three html file pages. 
+// It will contain the user's character object, as well as the functions
+// to save data to said object and open the next page in the application.
+
+$(document).ready(function(){
+    
+    // user character object
+    var character = {};
+
+    // save and continue function on page 1
+    function saveAndContinue1(){}
+
+    // save and continue function on page 2
+    function saveAndContinue2(){}
+
+    // populate character sheet from localStorage 
+    // or character object, not sure yet what will be most effective
+    // probably local storage.
+    function populateCharSheet(){}
+
+    // Icebox:
+    // save character sheet info *to* localStorage when changes are made
+    function saveCharacterEdits(){}
+
+});
